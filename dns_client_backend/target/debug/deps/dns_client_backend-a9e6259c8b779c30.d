baac2cbccca4b0dbe27d4161f47fe2b37a44ff64
Aden Tahir <aden.tahir7@gmail.com>
2023-05-05 18:46:27 +0500

    Moving files to their designated places

--- dns_client_backend/target/debug/deps/dns_client_backend-a9e6259c8b779c30.d
+++
@@ -1,5 +0,0 @@
-C:\DNS client\Dns_client\dns_client_backend\target\debug\deps\dns_client_backend-a9e6259c8b779c30.rmeta: src\main.rs
-
-C:\DNS client\Dns_client\dns_client_backend\target\debug\deps\dns_client_backend-a9e6259c8b779c30.d: src\main.rs
-
-src\main.rs:
